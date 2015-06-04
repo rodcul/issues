420cab8ddca1d34276f5c6b4574ae30b7da5292b
GJMcGowan <gemcgow@gmail.com>
2015-06-04 15:43:23 +0100

    Setting up github issues editing - remember to add auth key

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+server.listen(3000, function(){
+  console.log("Server listening on port 3000");
+});
+
+module.exports = server;
---
+++ test.js
@@ -0,0 +1,22 @@
+var token = "";
+
+var GitHubApi = require("github");
+
+var github = new GitHubApi({
+    version: "3.0.0",
+});
+
+github.authenticate({
+    type: "oauth",
+    token: token
+});
+
+github.issues.edit({
+    user: "rodcul",
+    repo: "issues",
+    number: 14,
+    labels: ["wf-2h"]
+}, function(err, res) {
+    console.log(JSON.stringify(err));
+    console.log(JSON.stringify(res));
+});
